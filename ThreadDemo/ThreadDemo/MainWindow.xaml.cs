429bab2c19032b96aaa7146453878573d6702751
huizengx <1511174656@qq.com>
2019-12-31 17:32:49 +0800

    update SearchFile

--- ThreadDemo/ThreadDemo/MainWindow.xaml.cs
+++ ThreadDemo/ThreadDemo/MainWindow.xaml.cs
@@ -51,8 +51,33 @@ namespace ThreadDemo
 
         private void ClearLogBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            FindAppFile($@"C:\Program Files (x86)");//
         }
+
+        //检索目录及子目录中的指定文件
+        private void FindAppFile(string path)
+        {
+            if (System.IO.Directory.Exists(path))
+            {
+                var files = System.IO.Directory.GetFiles(path).AsParallel();
+                Parallel.ForEach(files, (item) => {
+                    var Name = System.IO.Path.GetFileName(item);
+                    if (Name == "notepad++.exe")//
+                    {
+                        MessageBox.Show(item);
+                        return;
+                    }
+                });
+
+                var dirs = System.IO.Directory.GetDirectories(path);
+                foreach (var item in dirs)
+                {
+                    FindAppFile(item);
+                }
+            }
+            
+        }
+
 
         private void Thread_contend_Click(object sender, RoutedEventArgs e)
         {
